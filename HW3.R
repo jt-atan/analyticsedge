9f430cc5c52e5432245737f0236934691c838d41
stevelizcano <stephen.lizcano@gmail.com>
2017-07-02 20:15:48 +0200

    finished HW3

--- HW3.R
+++ HW3.R
@@ -137,5 +137,100 @@ table(loans$not.fully.paid)
 1533/(1533+8045)
 is.na(loans$pub.rec)
 
-#We need to impute the missing data
+#We need to impute the missing data - this is how you impute missing variables in R
 library(mice)
+set.seed(144)
+vars.for.imputation = setdiff(names(loans),'not.fully.paid')
+imputed = complete(mice(loans[vars.for.imputation]))
+loans[vars.for.imputation] = imputed
+
+#let's load the imputed data from the dataset to ensure accurate results
+loans = read.csv('loans_imputed.csv')
+
+#make prediction
+set.seed(144)
+
+#Here is how you do train-test-split in R and do a logistic regression in R
+split = sample.split(loans$not.fully.paid, SplitRatio = 0.7)
+train = subset(loans, split == TRUE)
+test = subset(loans, split == FALSE)
+
+loansModel1 = glm(not.fully.paid ~ ., data = train, family = binomial)
+summary(loansModel1)
+
+#What are the odds of repayment if person A has 700 FICO and person B has 710 FICO?
+(-.009317*700)-(-9.317*710)
+exp((-.009317*700)+9.187)-exp((-.009317*710)+9.187)
+#fuck! this is the right answer:
+-0.009317 * -10
+
+#2.3
+#predict the probabilities of the model.
+predicted.risk = predict(loansModel1, newdata = test, type = 'response')
+#confusion matrix
+table(test$not.fully.paid,predicted.risk >= 0.5)
+#accuracy
+(2400+3)/(2400+13+457+3)
+#baseline prediction
+table(test$not.fully.paid,predicted.risk >= 0.0)
+#we're supposed to add it?
+test$predicted.risk = predicted.risk
+
+#AUC calculation:
+library(ROCR)
+ROCRpreds = prediction(predicted.risk, test$not.fully.paid)
+auc = as.numeric(performance(ROCRpreds, 'auc')@y.values)
+auc
+
+#bivariate Logistic regression model (is a model with a single independent variable)
+bivariateModel = glm(not.fully.paid ~ int.rate, data = train, family = binomial)
+summary(bivariateModel)
+
+bvPredictions= predict(bivariateModel, newdata = test, type = 'response')
+max(bvPredictions)
+table(test$not.fully.paid, bvPredictions >= 0.0)
+summary(bvPredictions)
+#since it only had one column, no probabilities were over 0.5
+
+#auc again
+library(ROCR)
+ROCRpreds = prediction(bvPredictions, test$not.fully.paid)
+auc = as.numeric(performance(ROCRpreds, 'auc')@y.values)
+auc
+
+#How much does a 10 dollar investment with continuous compounding interest pay
+# back after 3 years? 6% interest
+10*exp(.06*3)
+
+
+#get profit on loans if you invest $1 in each of them 
+test$profit = exp(test$int.rate*3)-1
+#this is a good command to know, subtract -1 from profit where the loans are not fully paid
+test$profit[test$not.fully.paid == 1] -1
+max(test$profit)
+
+#subset high interest loans
+HighInterestTestSet = subset(test, int.rate >= 0.15)
+summary(HighInterestTestSet)
+HighInterestTestSet$profit = exp(HighInterestTestSet$int.rate*3) - 1
+HighInterestTestSet$profit[HighInterestTestSet$not.fully.paid == 1] -1
+summary(HighInterestTestSet)
+
+highInterest = subset(test, int.rate >= 0.15)
+summary(highInterest$profit)
+test$profit = exp(test$int.rate*3) - 1
+test$profit[test$not.fully.paid == 1] = -1
+
+#6.3
+'** IMPORTANT R COMMAND! this sorts a column by risk and returns a total value'
+cutoff = sort(highInterest$predicted.risk, decreasing = FALSE)[100]
+str(highInterest)
+str(cutoff)
+selectedLoans = subset(highInterest, predicted.risk <= cutoff)
+str(selectedLoans)
+#what is the profit of the investor of the top 100 loans for this cutoff predicted risk?
+summary(selectedLoans)
+sum(selectedLoans$profit)
+table(selectedLoans$not.fully.paid)
+str(selectedLoans)
+
